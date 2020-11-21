4b6b8b9ce260d091ea436503b8423051a4837c51
JAIDEEP_C <51963564+Jaideep-C@users.noreply.github.com>
2020-11-21 23:15:03 +0530

    Imports Update

--- lib/global/models/user.dart
+++ lib/global/models/user.dart
@@ -2,39 +2,39 @@
 //
 //     final user = userFromJson(jsonString);
 
-import 'package:attandee/global/models/absent.dart';
-import 'package:attandee/global/models/attended.dart';
+import 'package:attandee/global/models/miscellaneous/absent.dart';
 import 'package:meta/meta.dart';
 import 'dart:convert';
+import 'miscellaneous/attended.dart';
 
 User userFromJson(String str) => User.fromJson(json.decode(str));
 
 String userToJson(User data) => json.encode(data.toJson());
 
 class User {
-    User({
-        @required this.attended,
-        @required this.absent,
-        @required this.isAdmin,
-        @required this.attendancePercentage,
-        @required this.id,
-        @required this.batch,
-        @required this.fullName,
-        @required this.uniqueId,
-        @required this.emailId,
-    });
+  User({
+    @required this.attended,
+    @required this.absent,
+    @required this.isAdmin,
+    @required this.attendancePercentage,
+    @required this.id,
+    @required this.batch,
+    @required this.fullName,
+    @required this.uniqueId,
+    @required this.emailId,
+  });
 
-    final Attended attended;
-    final Absent absent;
-    final bool isAdmin;
-    final int attendancePercentage;
-    final String id;
-    final String batch;
-    final String fullName;
-    final String uniqueId;
-    final String emailId;
+  final Attended attended;
+  final Absent absent;
+  final bool isAdmin;
+  final int attendancePercentage;
+  final String id;
+  final String batch;
+  final String fullName;
+  final String uniqueId;
+  final String emailId;
 
-    factory User.fromJson(Map<String, dynamic> json) => User(
+  factory User.fromJson(Map<String, dynamic> json) => User(
         attended: Attended.fromJson(json["attended"]),
         absent: Absent.fromJson(json["absent"]),
         isAdmin: json["isAdmin"],
@@ -44,9 +44,9 @@ class User {
         fullName: json["fullName"],
         uniqueId: json["uniqueId"],
         emailId: json["emailId"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "attended": attended.toJson(),
         "absent": absent.toJson(),
         "isAdmin": isAdmin,
@@ -56,8 +56,5 @@ class User {
         "fullName": fullName,
         "uniqueId": uniqueId,
         "emailId": emailId,
-    };
+      };
 }
-
-
-
