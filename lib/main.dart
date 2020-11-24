0b219ab82c8ec76321ae94b752d2915db7b5eb72
Bhargav Kantheti <70275374+bharxhav@users.noreply.github.com>
2020-11-24 18:28:32 +0530

    Update Main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:attandee/global/screens/home_page.dart';
-import 'package:attandee/global/screens/login_page.dart';
+import 'package:attandee/global/screens/user_login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'global/models/token.dart';
@@ -27,7 +27,7 @@ class _IsAuthState extends State<IsAuth> {
     token.updateId();
     print("Token" + token.id.toString());
     return (token.id == '' || token.id == null)
-        ? LoginPage()
+        ? LoginScreen()
         : HomePage(token.id);
   }
 }
